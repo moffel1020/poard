d3639f4a0092c17d73143043f5892c1720d506af
moffel <moffel1020@gmail.com>
2022-06-21 18:04:48 +0200

    version

--- src/Window.cpp
+++ src/Window.cpp
@@ -13,8 +13,8 @@ Window::Window(int width, int height, int refreshRate, const char *title, bool f
 
 void Window::Initialize()
 {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWmonitor *monitor = (fullscreen) ? glfwGetPrimaryMonitor() : nullptr;
